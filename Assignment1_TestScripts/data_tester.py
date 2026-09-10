d83d12fab1ef9e6204ba440c3863ed528f3d26ad
Nick Dang <ndang7863@gmail.com>
2026-09-09 18:17:00 -0700

    Feature: Testing load_dataset

--- Assignment1_TestScripts/data_tester.py
+++ Assignment1_TestScripts/data_tester.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pandas as pd
 import torch as t
+from data_process1 import *
 import time
 
 
 if __name__ == "__main__":
-    '''
-    You can write your filled out functions in here to test them out.
-    '''
-    pass
+
+    GasProperties = load_dataset("GasProperties.csv")
+
+    print("TEST load_dataset")
+    print("Number of rows:", len(GasProperties))
+    print("First row:", GasProperties[0])
+    print("Type of dataset:", type(GasProperties))
+    print("Type of first value:", type(GasProperties[0][0]))
